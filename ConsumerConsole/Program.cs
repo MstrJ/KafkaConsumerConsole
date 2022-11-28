5a2f0cda1f88840cc6dac6a1c66e082c470314a9
MstrJ <jakubkruliczak@gmail.com>
2022-11-28 14:45:21 +0100

    Kafka consumer

---
+++ ConsumerConsole/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using Confluent.Kafka;
+using Newtonsoft.Json;
+
+record Post(int Id,string Title,string Content);
+class Program
+{
+    public static async Task Main()
+    {
+        var config = new ConsumerConfig
+        {
+            BootstrapServers = "localhost:9094",
+            GroupId = "newPost",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+
+        using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+        {
+            consumer.Subscribe(new string[] {"addedPosts"});
+            bool cancelled = false;
+            while (!cancelled)
+            {
+                var consumeResult = consumer.Consume();
+                // process message here.
+
+                    try
+                    {
+                        consumer.Commit(consumeResult);
+                        Console.WriteLine($"{consumeResult.Value}");
+                    }
+                    catch (KafkaException e)
+                    {
+                        Console.WriteLine($"Commit error: {e.Error.Reason}");
+                    }
+            }
+            consumer.Close();
+        }
+    }
+}
